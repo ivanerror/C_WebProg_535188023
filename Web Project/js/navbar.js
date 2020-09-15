97302b519e33e3c819563ad7d7340c9d7a6daebf
ivanerror <ivangabriel68@gmail.com>
2020-09-15 21:32:23 +0700

    add javascript

---
+++ Web Project/js/navbar.js
@@ -0,0 +1,10 @@
+$(window).scroll(function () {
+  $(".navbar").toggleClass("scrolled", $(this).scrollTop() > 300);
+  $(".navbar a").toggleClass("scrolled", $(this).scrollTop() > 300);
+
+  if ($(this).scrollTop() < 300) {
+    $(".navbar .navbar-brand img").attr("src", "accel-logo.png");
+  } else {
+    $(".navbar .navbar-brand img").attr("src", "accel-logo-scrolled.png");
+  }
+});
