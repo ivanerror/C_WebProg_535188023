652750e5096e49e701c00f0757622edfde03e6ca
ivanerror <ivangabriel68@gmail.com>
2020-09-16 18:25:50 +0700

    New Navbar Fusion

--- Web Project/js/navbar.js
+++ Web Project/js/navbar.js
@@ -8,3 +8,19 @@ $(window).scroll(function () {
     $(".navbar .navbar-brand img").attr("src", "accel-logo-scrolled.png");
   }
 });
+
+$(window).on("resize load", function () {
+  if ($(window).width() < 992) {
+    $("#search-box").addClass("search-box");
+    $("#search-txt").addClass("search-txt");
+
+    $("#search-box").removeClass("search-box-hover");
+    $("#search-txt").removeClass("search-txt-hover");
+  } else {
+    $("#search-box").addClass("search-box-hover");
+    $("#search-txt").addClass("search-txt-hover");
+
+    $("#search-box").removeClass("search-box");
+    $("#search-txt").removeClass("search-txt");
+  }
+});
