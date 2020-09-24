39d0c890614daffa68359df04a5181fd3add817c
dreamingkid <danremuranas@gmail.com>
2020-09-24 18:21:14 +0700

    styling

--- Homepage Tugas/js/script.js
+++ Homepage Tugas/js/script.js
@@ -24,18 +24,11 @@ $(document).ready(function () {
   textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
   anime
-    .timeline({ loop: true })
+    .timeline({ loop: false })
     .add({
       targets: ".ml10 .letter",
       rotateY: [-90, 0],
       duration: 1300,
       delay: (el, i) => 45 * i,
     })
-    .add({
-      targets: ".ml10",
-      opacity: 0,
-      duration: 1000,
-      easing: "easeOutExpo",
-      delay: 1000,
-    });
 });
