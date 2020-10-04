4499b6d7fb4b7243ab57e4c33a4d7823918f0809
hendrawan30 <hendrawancahyady@gmail.com>
2020-10-04 13:43:20 +0700

    Overlay

--- Web Project/js/navbar.js
+++ Web Project/js/navbar.js
@@ -9,3 +9,15 @@ $(window).scroll(function () {
     $("#search-box").hide();
   }
 });
+
+$(document).ready(function(){
+  $("#heart").click(function(){
+    if($("#heart").hasClass("liked")){
+      $("#heart").html('<i class="fa fa-heart-o fa-lg" aria-hidden="true"></i>');
+      $("#heart").removeClass("liked");
+    }else{
+      $("#heart").html('<i class="fa fa-heart fa-lg" aria-hidden="true"></i>');
+      $("#heart").addClass("liked");
+    }
+  });
+});
