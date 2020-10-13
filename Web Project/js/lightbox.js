4c71f0b31442b64ad68964d024508f50c394aea6
ivanerror <ivangabriel68@gmail.com>
2020-10-14 06:34:02 +0700

    push

--- Web Project/js/lightbox.js
+++ Web Project/js/lightbox.js
@@ -4,12 +4,33 @@ const box = document.querySelectorAll(".box");
 
 box.forEach((image) => {
   image.addEventListener("click", (e) => {
-    lightbox.classList.add("active");
-    document.querySelector("#img-modal").src = image.querySelector("img").src;
+    if(e.target.classList[0] == 'content') {
+      lightbox.classList.add("active");
+      document.querySelector("#img-modal").src = image.querySelector("img").src;
+    }
   });
 });
 
 lightbox.addEventListener("click", (e) => {
   if (e.target !== e.currentTarget) return;
   lightbox.classList.remove("active");
+});
+
+const fillHeart = '<i class="fa fa-heart fa-lg" aria-hidden="true"></i>';
+const outlineHeart = '<i class="fa fa-heart-o fa-lg" aria-hidden="true"></i>';
+
+const heartIcon = document.querySelectorAll('.heart-icon');
+
+console.log(heartIcon)
+
+heartIcon.forEach((icon) => {
+  icon.addEventListener('click', (e) => {
+    if($(icon).hasClass('liked')){
+      $(icon).html(outlineHeart)
+      $(icon).removeClass('liked')
+    } else {
+      $(icon).html(fillHeart)
+      $(icon).addClass('liked')
+    }
+  });
 });
