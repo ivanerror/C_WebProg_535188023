634e8e3bb9a8b98c6e0b3510827cc1e2d13ea634
rekiyagami <39211506+rekiyagami@users.noreply.github.com>
2020-11-15 17:19:06 +0700

    server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+// Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + '/public/css'))
+app.use('/js', express.static(__dirname + '/public/js'))
+app.use('/img', express.static(__dirname + '/public/img'))
+
+// Set Views
+app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
