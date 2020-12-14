05ce84271790651c16252099883761c1da52cd56
davidraharja <70616495+davidraharja@users.noreply.github.com>
2020-12-14 14:51:40 +0700

    Final Deploy

--- backend/routes/insframeProfile.js
+++ backend/routes/insframeProfile.js
@@ -5,10 +5,14 @@ const Image = require("../models/image");
 const Category = require("../models/category");
 const auth = require("./auth");
 const user = require("../models/user");
-const { update } = require("../models/image");
+const { update, count } = require("../models/image");
 const { route } = require("./insframeAPI");
+const { image } = require("../cloudinaryConfig");
+const multer = require("multer");
+const upload = require("../multerConfig");
+const cloudinary = require("../cloudinaryConfig");
 
-router.get("/@:username", auth.checkAuthNext, async (req, res) => {
+router.get("/collection/@:username", auth.checkAuthNext, async (req, res) => {
   username = req.params.username;
   var User = {};
   var myUser = {};
@@ -19,6 +23,7 @@ router.get("/@:username", auth.checkAuthNext, async (req, res) => {
     viewUser = await user.findOne({
       username: username,
     });
+    countPhoto = await Image.find({author : req.user.id})
     images = await Image.find({
       collect_by: viewUser._id,
     }).populate("author", "username email biography img_profile");
@@ -33,6 +38,47 @@ router.get("/@:username", auth.checkAuthNext, async (req, res) => {
       if (!viewUser || !images) throw new Error("...");
     }
     res.render("profile", {
+      count : countPhoto.length,
+      tabMode : "collection",
+      logged: logged,
+      User: User,
+      viewUser: viewUser,
+      imageList: images,
+      isMyProfile : isMyProfile
+    });
+  } catch (error) {
+    // res.redirect("/404");
+    res.json({error:error.message})
+  }
+});
+
+router.get("/photos/@:username", auth.checkAuthNext, async (req, res) => {
+  username = req.params.username;
+  var User = {};
+  var myUser = {};
+  var logged = false;
+  var isMyProfile = false;
+
+  try {
+    viewUser = await user.findOne({
+      username: username,
+    });
+    images = await Image.find({
+      author: viewUser._id,
+    }).populate("author", "username email biography img_profile");
+    if (req.isAuthenticated) {
+      User = await user.findOne({
+        _id: req.user.id
+      });
+      if (viewUser._id == req.user.id) isMyProfile = true;
+      logged = true
+      if (!viewUser || !images || !User) throw new Error("...");
+    } else {
+      if (!viewUser || !images) throw new Error("...");
+    }
+    res.render("profile", {
+      count : images.length,
+      tabMode : "photo",
       logged: logged,
       User: User,
       viewUser: viewUser,
@@ -56,6 +102,50 @@ router.get("/edit", auth.checkAuth, async (req, res) => {
     res.redirect("/404");
   }
 });
+
+const multerConf = {
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/img/uploads");
+    },
+    filename: function (req, file, cb) {
+      const parts = file.mimetype.split("/")[1];
+      cb(null, file.fieldname + "-" + Date.now() + "." + parts);
+    },
+    fileFilter: function (req, file, next) {
+      if (!file) {
+        cb();
+      }
+      const image = file.mimetype.startsWith("image/");
+      if (image) {
+        cb({ message: "File Done" }, true);
+      } else {
+        cb({ message: "File type not supported" }, false);
+      }
+    },
+  }),
+};
+
+router.post(
+  "/uploadimg",
+  multer(multerConf).single("photo"), auth.checkAuthNext,
+  async (req, res) => {
+    if (req.file) {
+      req.body.photo = req.file.filename;
+      const result = await cloudinary.uploader.upload(
+        "./public/img/uploads/" + req.file.filename
+      );
+      const id = req.user.id;
+      const updated = await user.findOneAndUpdate(
+        { _id: id },
+        {
+          img_profile: result.url,
+        }
+      );
+      res.redirect('/profile/edit')
+    }
+  }
+);
 
 router.post("/update", auth.checkAuth, async (req, res) => {
   try {
